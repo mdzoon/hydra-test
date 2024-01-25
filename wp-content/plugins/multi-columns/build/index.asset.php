b8e0507a0c1a39c641ecbfcea61a9caeeae09b81
mdzoon <michaeldyczkowski@gmail.com>
2024-01-25 14:21:42 +0100

    removed unecessary files

--- wp-content/plugins/multi-columns/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '825d9606a589b5ba3749');
--- wp-content/plugins/multi-columns/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '23f2dbfd51e6398700a9');
